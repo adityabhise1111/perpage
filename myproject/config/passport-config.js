a07f55bede03a72bf44435c2d9a7b715ce38366f
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2025-06-10 20:21:28 +0000

    google env url

--- myproject/config/passport-config.js
+++ myproject/config/passport-config.js
@@ -34,7 +34,7 @@ export default function initialize(passport) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
   }, async (accessToken, refreshToken, profile, done) => {
   let user = await User.findOne({ googleId: profile.id });
   if (!user) {
